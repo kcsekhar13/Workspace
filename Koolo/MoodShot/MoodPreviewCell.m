c7610a8181a2f85cc9172efc975ab580163390f6
VinodRamios <vinodram.ios@gmail.com>
2015-11-01 13:08:48 +0530

    Modifications done in moods

---
+++ Koolo/MoodShot/MoodPreviewCell.m
@@ -0,0 +1,23 @@
+//
+//  MoodPreviewCell.m
+//  Koolo
+//
+//  Created by CNU on 01/11/15.
+//  Copyright © 2015 Vinodram. All rights reserved.
+//
+
+#import "MoodPreviewCell.h"
+
+@implementation MoodPreviewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
