cded62508eae3513b8b19a94d1c2f7da2c39c5e5
VinodRamios <vinodram.ios@gmail.com>
2015-11-04 23:43:44 +0530

    Done the UI changes in check list home screen

--- Koolo/Checklists/CheckListViewController.m
+++ Koolo/Checklists/CheckListViewController.m
@@ -35,18 +35,21 @@
     _myHealthButton.titleLabel.textAlignment = NSTextAlignmentCenter;
     _myHealthButton.titleLabel.numberOfLines = 2;
     [_myHealthButton.layer setMasksToBounds:YES];
+    [_myHealthButton setFrame:CGRectMake(_myHealthButton.frame.origin.x, _myHealthButton.frame.origin.y, _myHealthButton.frame.size.width, _myHealthButton.frame.size.width)];
     
     [_threeSentence.layer setBorderColor:[UIColor clearColor].CGColor];
     _threeSentence.titleLabel.textAlignment = NSTextAlignmentCenter;
     _threeSentence.titleLabel.numberOfLines = 2;
     [_threeSentence.layer setCornerRadius:_myHealthButton.frame.size.width/2];
     [_threeSentence.layer setMasksToBounds:YES];
+    [_threeSentence setFrame:CGRectMake(_threeSentence.frame.origin.x, _threeSentence.frame.origin.y, _threeSentence.frame.size.width, _threeSentence.frame.size.width)];
     
     [_readyButton.layer setBorderColor:[UIColor clearColor].CGColor];
     [_readyButton.layer setCornerRadius:_myHealthButton.frame.size.width/2];
     _readyButton.titleLabel.textAlignment = NSTextAlignmentCenter;
     _readyButton.titleLabel.numberOfLines = 2;
     [_readyButton.layer setMasksToBounds:YES];
+    [_readyButton setFrame:CGRectMake(_readyButton.frame.origin.x, _readyButton.frame.origin.y, _readyButton.frame.size.width, _readyButton.frame.size.width)];
     // Do any additional setup after loading the view.
 }
 
