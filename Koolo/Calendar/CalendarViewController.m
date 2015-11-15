1911fdfb266760808ac6a9dbb25f47cdc33563fc
VinodRamios <vinodram.ios@gmail.com>
2015-11-15 20:36:43 +0530

    Added custom Calendar view

--- Koolo/Calendar/CalendarViewController.m
+++ Koolo/Calendar/CalendarViewController.m
@@ -7,10 +7,15 @@
 //
 
 #import "CalendarViewController.h"
+#import "CLWeeklyCalendarView.h"
 
-@interface CalendarViewController ()
+@interface CalendarViewController () <CLWeeklyCalendarViewDelegate>
 
+@property (weak, nonatomic) IBOutlet UIImageView *backgroundImageView;
+@property (nonatomic, strong) CLWeeklyCalendarView* calendarView;
 @end
+
+static CGFloat CALENDER_VIEW_HEIGHT = 150.f;
 
 @implementation CalendarViewController
 
@@ -18,11 +23,86 @@
     self.navigationController.navigationBar.hidden = NO;
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    NSString * language = [[NSLocale preferredLanguages] objectAtIndex:0];
+    NSString *doneButtonTitle = nil;
+    if ([language isEqualToString:@"nb"]) {
+        
+        self.title = NSLocalizedString(@"Calendar", nil);
+        doneButtonTitle = NSLocalizedString(@"Ready", nil);
+        
+    } else {
+        self.title = @"Calendar";
+        doneButtonTitle = @"Finished";
+    }
+    
+    
+    dataManager = [StoreDataMangager sharedInstance];
+    UIImage *backgroundImage = dataManager.returnBackgroundImage;
+    if (backgroundImage) {
+        _backgroundImageView.image = backgroundImage;
+    }
+    
+    // create blur effect
+    UIBlurEffect *blur = [UIBlurEffect effectWithStyle:UIBlurEffectStyleLight];
+    
+    // create vibrancy effect
+    UIVibrancyEffect *vibrancy = [UIVibrancyEffect effectForBlurEffect:blur];
+    
+    // add blur to an effect view
+    UIVisualEffectView *effectView = [[UIVisualEffectView alloc]initWithEffect:blur];
+    effectView.frame = self.view.frame;
+    
+    // add vibrancy to yet another effect view
+    UIVisualEffectView *vibrantView = [[UIVisualEffectView alloc]initWithEffect:vibrancy];
+    vibrantView.frame = self.view.frame;
+    effectView.alpha = 0.9;
+    vibrantView.alpha = 0.9;
+    // add both effect views to the image view
+    [self.backgroundImageView addSubview:effectView];
+    [self.backgroundImageView addSubview:vibrantView];
+    
+    [self.navigationItem setHidesBackButton:YES animated:NO];
+    
+    UIBarButtonItem* doneButton = [[UIBarButtonItem alloc] initWithTitle:doneButtonTitle style:UIBarButtonItemStylePlain target:self action:@selector(moveToHome)];
+    [doneButton setTitleTextAttributes:[NSDictionary dictionaryWithObjectsAndKeys:[UIColor blackColor],NSForegroundColorAttributeName, nil] forState:UIControlStateNormal];
+    self.navigationItem.rightBarButtonItem = doneButton;
+    
+    [self.view addSubview:self.calendarView];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+#pragma mark - CLWeeklyCalendarView Initialize and Delegate methods
+
+//Initialize
+-(CLWeeklyCalendarView *)calendarView
+{
+    if(!_calendarView){
+        _calendarView = [[CLWeeklyCalendarView alloc] initWithFrame:CGRectMake(0, 66, self.view.bounds.size.width, CALENDER_VIEW_HEIGHT)];
+        _calendarView.delegate = self;
+    }
+    return _calendarView;
+}
+
+-(NSDictionary *)CLCalendarBehaviorAttributes
+{
+    return @{
+             CLCalendarWeekStartDay : @2,                 //Start Day of the week, from 1-7 Mon-Sun -- default 1
+             //             CLCalendarDayTitleTextColor : [UIColor yellowColor],
+             //             CLCalendarSelectedDatePrintColor : [UIColor greenColor],
+             };
+}
+
+
+
+-(void)dailyCalendarViewDidSelect:(NSDate *)date
+{
+    //You can do any logic after the view select the date
 }
 
 /*
@@ -35,4 +115,10 @@
 }
 */
 
+#pragma mark - User defined methods
+
+- (void)moveToHome {
+    
+    [self.navigationController popViewControllerAnimated:YES];
+}
 @end
