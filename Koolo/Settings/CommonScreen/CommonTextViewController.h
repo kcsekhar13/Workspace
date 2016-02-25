007d3b49513a69fe96ac44eb93ed4550b8e9f567
VinodRamios <vinodram.ios@gmail.com>
2016-02-25 22:56:22 +0530

    Added resources files for About, Contribution and License screens

---
+++ Koolo/Settings/CommonScreen/CommonTextViewController.h
@@ -0,0 +1,13 @@
+//
+//  CommonTextViewController.h
+//  Koolo
+//
+//  Created by Hamsini on 25/02/16.
+//  Copyright © 2016 Vinodram. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CommonTextViewController : UIViewController
+
+@end
